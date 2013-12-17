66c7c5b2541bda7005285755f1c40b7c8c94d13e
Grant Kemp <grant@kempnet.com>
2013-12-17 00:56:18 +0000

    updated class path and project settings so each project isn’t called “main Activity” and causes issues when importing all the projects at once.

---
+++ eclipse_compat/libraries/BaseGameUtils/gen/com/google/example/games/basegameutils/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.example.games.basegameutils;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
