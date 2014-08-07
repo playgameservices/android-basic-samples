6f3653b1f1ccd9e1de6b84ba8675657d7762dc52
Ian Ni-Lewis <inl@google.com>
2014-08-07 15:29:34 -0700

    Build with a specific GMS Core branch
    note that this cherry-pick includes some files marked DO_NOT_MERGE.
    This is intentional. These files were developed in testbranch and
    are being moved to master. They should NOT be merged from master to
    any branch with more visibility.
    
    Conflicts:
    	build.sh
    	deploy_local.py
    	install-dogfood-gms.py
    
    Change-Id: Ie3f5eebdf98cff069d5b91cfa4f3c20c64f82d3f

---
+++ DO_NOT_MERGE/README.txt
@@ -0,0 +1,1 @@
+Built from google3/experimental/ilewis/dogfood_gms:*
--- build.sh
+++ build.sh
@@ -1,6 +1,40 @@
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-export PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/bin
+set -e
+set -u
+echo Setting environment variables...
+: ${ANDROID_HOME:=/opt/android-sdk}
+
+
+JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/bin
+
+echo Copying Android SDK...
+ANDROID_SDK_COPY=`pwd`/android-sdk
+if [ -d $ANDROID_SDK_COPY -o -e $ANDROID_SDK_COPY ] 
+then
+  rm -rf $ANDROID_SDK_COPY
+fi
+echo "$ANDROID_HOME --> $ANDROID_SDK_COPY"
+mkdir $ANDROID_SDK_COPY
+#mkdir $ANDROID_SDK_COPY/extras
+for DIR in $( ls $ANDROID_HOME )
+do
+  if [ $DIR != "extras" ]
+  then
+    ln -s $ANDROID_HOME/$DIR $ANDROID_SDK_COPY/$DIR
+  fi
+done
+cp -r $ANDROID_HOME/extras $ANDROID_SDK_COPY/extras
+ANDROID_HOME=$ANDROID_SDK_COPY
+
+echo Running branch-specific build steps...
+if [ -e .branch_build ]
+then
+  . .branch_build
+fi
+
+echo Building...
 cd BasicSamples
-echo "sdk.dir=/opt/android-sdk" >local.properties 
+echo "sdk.dir=$ANDROID_HOME" >local.properties 
+
 ./gradlew --stacktrace assembleDebug
 
