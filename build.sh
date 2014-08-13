5d9327e2f1c81821d2257ccca9846c205cb827db
Ian Ni-Lewis <ilewis@google.com>
2014-08-13 22:41:05 +0000

    Revert "Correct various build script issues raised by smiles@"
    This reverts commit 49a52f900ebc94a35b053993aecfb372f4ab850e.
    
    Change-Id: I826e7c28a643d56d9349646ae93182c5f8f69ac9

--- build.sh
+++ build.sh
@@ -1,24 +1,40 @@
-#!/bin/bash -eu
-
-# Set environment variables
-declare -r script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+set -e
+set -u
+echo Setting environment variables...
 : ${ANDROID_HOME:=/opt/android-sdk}
-: ${JAVA_HOME:=/usr/lib/jvm/java-7-openjdk-amd64}
-PATH=$PATH:$JAVA_HOME/bin
 
+
+JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/bin
+
+echo Copying Android SDK...
+ANDROID_SDK_COPY=`pwd`/android-sdk
+if [ -d $ANDROID_SDK_COPY -o -e $ANDROID_SDK_COPY ] 
+then
+  rm -rf $ANDROID_SDK_COPY
+fi
+echo "$ANDROID_HOME --> $ANDROID_SDK_COPY"
+mkdir $ANDROID_SDK_COPY
+#mkdir $ANDROID_SDK_COPY/extras
+for DIR in $( ls $ANDROID_HOME )
+do
+  if [ $DIR != "extras" ]
+  then
+    ln -s $ANDROID_HOME/$DIR $ANDROID_SDK_COPY/$DIR
+  fi
+done
+cp -r $ANDROID_HOME/extras $ANDROID_SDK_COPY/extras
+ANDROID_HOME=$ANDROID_SDK_COPY
 
 echo Running branch-specific build steps...
-if [ -e "${script_dir}/branch_build.sh" ]; then
-  . "${script_dir}/branch_build.sh"
+if [ -e .branch_build ]
+then
+  . .branch_build
 fi
 
-cd "${script_dir}/BasicSamples"
+echo Building...
+cd BasicSamples
+echo "sdk.dir=$ANDROID_HOME" >local.properties 
 
-# The Android build system wants to see a local.properties file that tells
-# it where to find the Android SDK. 
-echo "sdk.dir=${ANDROID_HOME}" >local.properties 
-
-# Build all samples.
 ./gradlew --stacktrace assembleDebug
 
